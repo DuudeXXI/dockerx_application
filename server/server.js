b350d00e2009646a324377331421b7b407cfc7bf
DuudeXXI <106966677+DuudeXXI@users.noreply.github.com>
2023-07-31 11:21:20 +0300

    init

---
+++ server/server.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const mysql = require("mysql2");
+const cors = require("cors");
+const { v4: uuid } = require("uuid");
+const bodyParser = require("body-parser");
+
+app.use(cors());
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extend: true }));
+app.use(express.static(__dirname + "/public/styles"));
+app.use(express.json());
+app.use(express.static("public"));
+
+const connection = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  database: "tempo_db",
+  password: "Zbjebt262675",
+  multipleStatements: true
+});
+
+// app.get("/", (req, res) => {
+//   const sql = "SELECT COUNT(*) AS count FROM controllers";
+//   connection.query(sql, (err, results) => {
+//     if (err) throw err;
+
+//     const count = results[0].count;
+//     // res.sendFile(__dirname + "/index.html");
+//     res.json(count);
+//   });
+// });
+app.get("/", (req, res) => {
+  let sql = `SELECT COUNT(*) AS count FROM controllers;
+             SELECT * FROM controllers AS data_test;`
+  connection.query(sql, (err, results) => {
+    if (err) throw err;
+
+    const count = results;
+    res.json(count);
+});
+});
+
+app.get("/api/", (req, res) => {
+
+  const controllerId = req.query.id;
+  const sql = `SELECT * FROM controllers WHERE id = ?;`
+  const values = [controllerId];
+
+  connection.query(sql,values,(err, results) => {
+    if (err) throw err;
+
+    res.json(results);
+});
+});
+
+app.post("/register", (req, res) => {
+  const sql = `INSERT INTO controllers SET ?`;
+  const controller = {
+    dec_lat: req.body.dec_lat,
+    dec_lng: req.body.dec_lng,
+  };
+  console.log(controller.dec_lat.length);
+  if (!isNaN(controller.dec_lat) &&
+      !isNaN(controller.dec_lng) &&
+      controller.dec_lat.length > 1 &&
+      controller.dec_lng.length > 1) {
+    connection.query(sql, controller, (err, results) => {
+      if (err) throw err
+      console.log("Sent successfully");
+    });
+  } else {
+    console.log("kazkas negerai");
+  }
+});
+
+app.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const { dec_lat, dec_lng, controler_status } = req.body;
+
+  const query = 'UPDATE controllers SET dec_lat = ?, dec_lng = ?, controler_status = ? WHERE id = ?';
+  const values = [dec_lat, dec_lng, controler_status, id];
+
+  connection.query(query, values, (err, result) => {
+    if (err) {
+      console.error('Error updating data:', err);
+      res.status(500).json({ error: 'Error updating data' });
+    } else {
+      console.log('Data updated successfully');
+      res.json({ message: 'Data updated successfully' });
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log("API Service running...");
+});
