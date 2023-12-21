58c01aedd8ef819bf932011d8212aebe7cabde68
DuudeXXI <106966677+DuudeXXI@users.noreply.github.com>
2023-12-21 18:04:16 +0200

    navigation container default visual styling completed

--- client/src/Components/MainNavigation.jsx
+++ client/src/Components/MainNavigation.jsx
@@ -2,6 +2,7 @@ import "../Styles/MainNavigation.scss";
 import { elementHeight } from "../Resources/elementHeight";
 import nav_figure from "../Media/nav_figure.svg";
 import nav_bicycle from "../Media/nav_bicycle.svg";
+import info_icon from "../Media/info_icon.svg"
 // redux
 import { useSelector } from "react-redux";
 import { selectStation } from "../Reducers/selectedStationRecuder";
@@ -21,12 +22,32 @@ const MainNavigation = () => {
       {Boolean(station) ? (
         <>
           <div className="station-data-container">
-            content
+            <div className="primary">
+              <div className="image"></div>
+              <div className="text">DOK093</div>
+            </div>
+            <div className="station-data">
+              <div className="status">
+                Stat<div className="pseudo-label">Status</div>
+              </div>
+              <div className="type">
+                Type <div className="pseudo-label">Type</div>
+              </div>
+              <div className="occupancy">
+                3 <div className="pseudo-label">availability</div>
+              </div>
+            </div>
+              <div className="payment-container">
+                <div className="pricing">
+                  <div className="price-title">Pay as you go</div>
+                  <div className="price">0.12E/min</div>
+                </div>
+                <div className="info-icon"><img src={info_icon} /></div>
+              </div>
           </div>
-          <div className="line"></div>
           <div className="buttons-container">
-            <button className="lock-btn">Atrakinti</button>
-            <button className="reserve-btn">Užrakinti</button>
+            <button className="lock-btn">Lock</button>
+            <button className="reserve-btn">Reserve</button>
           </div>
         </>
       ) : null}
